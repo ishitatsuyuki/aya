abc600767548218e7e8f3f31c11336a04432bae5
Tatsuyuki Ishi <ishitatsuyuki@gmail.com>
2022-05-29 12:13:50 +0900

    bpf: Replace map types to use &self, remove HashMap::get_mut
    The bpf_map_defs are now wrapped with UnsafeCell, which also happens to
    provide a cozy way to get a *mut pointer. An UnsafeCell isn't strictly
    required as the struct fields are practically opaque to us, but using an
    UnsafeCell follows the general best practices against miscompilation and
    also prevents some obvious errors.
    
    HashMap::get_mut was removed because the idea is completely unsound.
    Previous users should wrap their data with UnsafeCell instead or use
    atomics.
    
    Closes: https://github.com/aya-rs/aya/issues/233

--- bpf/aya-bpf/src/maps/hash_map.rs
+++ bpf/aya-bpf/src/maps/hash_map.rs
@@ -1,4 +1,4 @@
-use core::{marker::PhantomData, mem, ptr::NonNull};
+use core::{cell::UnsafeCell, marker::PhantomData, mem, ptr::NonNull};
 
 use aya_bpf_bindings::bindings::bpf_map_type::{
     BPF_MAP_TYPE_LRU_HASH, BPF_MAP_TYPE_LRU_PERCPU_HASH, BPF_MAP_TYPE_PERCPU_HASH,
@@ -13,15 +13,22 @@ use crate::{
 
 #[repr(transparent)]
 pub struct HashMap<K, V> {
-    def: bpf_map_def,
+    def: UnsafeCell<bpf_map_def>,
     _k: PhantomData<K>,
     _v: PhantomData<V>,
 }
 
+unsafe impl<K: Sync, V: Sync> Sync for HashMap<K, V> {}
+
 impl<K, V> HashMap<K, V> {
     pub const fn with_max_entries(max_entries: u32, flags: u32) -> HashMap<K, V> {
         HashMap {
-            def: build_def::<K, V>(BPF_MAP_TYPE_HASH, max_entries, flags, PinningType::None),
+            def: UnsafeCell::new(build_def::<K, V>(
+                BPF_MAP_TYPE_HASH,
+                max_entries,
+                flags,
+                PinningType::None,
+            )),
             _k: PhantomData,
             _v: PhantomData,
         }
@@ -29,44 +36,51 @@ impl<K, V> HashMap<K, V> {
 
     pub const fn pinned(max_entries: u32, flags: u32) -> HashMap<K, V> {
         HashMap {
-            def: build_def::<K, V>(BPF_MAP_TYPE_HASH, max_entries, flags, PinningType::ByName),
+            def: UnsafeCell::new(build_def::<K, V>(
+                BPF_MAP_TYPE_HASH,
+                max_entries,
+                flags,
+                PinningType::ByName,
+            )),
             _k: PhantomData,
             _v: PhantomData,
         }
     }
 
     #[inline]
-    pub fn get(&mut self, key: &K) -> Option<&V> {
-        get(&mut self.def, key)
+    pub fn get(&self, key: &K) -> Option<&V> {
+        get(self.def.get(), key)
     }
 
     #[inline]
-    pub fn get_mut(&mut self, key: &K) -> Option<&mut V> {
-        get_mut(&mut self.def, key)
+    pub fn insert(&self, key: &K, value: &V, flags: u64) -> Result<(), c_long> {
+        insert(self.def.get(), key, value, flags)
     }
 
     #[inline]
-    pub fn insert(&mut self, key: &K, value: &V, flags: u64) -> Result<(), c_long> {
-        insert(&mut self.def, key, value, flags)
-    }
-
-    #[inline]
-    pub fn remove(&mut self, key: &K) -> Result<(), c_long> {
-        remove(&mut self.def, key)
+    pub fn remove(&self, key: &K) -> Result<(), c_long> {
+        remove(self.def.get(), key)
     }
 }
 
 #[repr(transparent)]
 pub struct LruHashMap<K, V> {
-    def: bpf_map_def,
+    def: UnsafeCell<bpf_map_def>,
     _k: PhantomData<K>,
     _v: PhantomData<V>,
 }
 
+unsafe impl<K: Sync, V: Sync> Sync for LruHashMap<K, V> {}
+
 impl<K, V> LruHashMap<K, V> {
     pub const fn with_max_entries(max_entries: u32, flags: u32) -> LruHashMap<K, V> {
         LruHashMap {
-            def: build_def::<K, V>(BPF_MAP_TYPE_LRU_HASH, max_entries, flags, PinningType::None),
+            def: UnsafeCell::new(build_def::<K, V>(
+                BPF_MAP_TYPE_LRU_HASH,
+                max_entries,
+                flags,
+                PinningType::None,
+            )),
             _k: PhantomData,
             _v: PhantomData,
         }
@@ -74,54 +88,51 @@ impl<K, V> LruHashMap<K, V> {
 
     pub const fn pinned(max_entries: u32, flags: u32) -> LruHashMap<K, V> {
         LruHashMap {
-            def: build_def::<K, V>(
+            def: UnsafeCell::new(build_def::<K, V>(
                 BPF_MAP_TYPE_LRU_HASH,
                 max_entries,
                 flags,
                 PinningType::ByName,
-            ),
+            )),
             _k: PhantomData,
             _v: PhantomData,
         }
     }
 
     #[inline]
-    pub fn get(&mut self, key: &K) -> Option<&V> {
-        get(&mut self.def, key)
+    pub fn get(&self, key: &K) -> Option<&V> {
+        get(self.def.get(), key)
     }
 
     #[inline]
-    pub fn get_mut(&mut self, key: &K) -> Option<&mut V> {
-        get_mut(&mut self.def, key)
+    pub fn insert(&self, key: &K, value: &V, flags: u64) -> Result<(), c_long> {
+        insert(self.def.get(), key, value, flags)
     }
 
     #[inline]
-    pub fn insert(&mut self, key: &K, value: &V, flags: u64) -> Result<(), c_long> {
-        insert(&mut self.def, key, value, flags)
-    }
-
-    #[inline]
-    pub fn remove(&mut self, key: &K) -> Result<(), c_long> {
-        remove(&mut self.def, key)
+    pub fn remove(&self, key: &K) -> Result<(), c_long> {
+        remove(self.def.get(), key)
     }
 }
 
 #[repr(transparent)]
 pub struct PerCpuHashMap<K, V> {
-    def: bpf_map_def,
+    def: UnsafeCell<bpf_map_def>,
     _k: PhantomData<K>,
     _v: PhantomData<V>,
 }
 
+unsafe impl<K: Sync, V: Sync> Sync for PerCpuHashMap<K, V> {}
+
 impl<K, V> PerCpuHashMap<K, V> {
     pub const fn with_max_entries(max_entries: u32, flags: u32) -> PerCpuHashMap<K, V> {
         PerCpuHashMap {
-            def: build_def::<K, V>(
+            def: UnsafeCell::new(build_def::<K, V>(
                 BPF_MAP_TYPE_PERCPU_HASH,
                 max_entries,
                 flags,
                 PinningType::None,
-            ),
+            )),
             _k: PhantomData,
             _v: PhantomData,
         }
@@ -129,54 +140,51 @@ impl<K, V> PerCpuHashMap<K, V> {
 
     pub const fn pinned(max_entries: u32, flags: u32) -> PerCpuHashMap<K, V> {
         PerCpuHashMap {
-            def: build_def::<K, V>(
+            def: UnsafeCell::new(build_def::<K, V>(
                 BPF_MAP_TYPE_PERCPU_HASH,
                 max_entries,
                 flags,
                 PinningType::ByName,
-            ),
+            )),
             _k: PhantomData,
             _v: PhantomData,
         }
     }
 
     #[inline]
-    pub fn get(&mut self, key: &K) -> Option<&V> {
-        get(&mut self.def, key)
+    pub fn get(&self, key: &K) -> Option<&V> {
+        get(self.def.get(), key)
     }
 
     #[inline]
-    pub fn get_mut(&mut self, key: &K) -> Option<&mut V> {
-        get_mut(&mut self.def, key)
+    pub fn insert(&self, key: &K, value: &V, flags: u64) -> Result<(), c_long> {
+        insert(self.def.get(), key, value, flags)
     }
 
     #[inline]
-    pub fn insert(&mut self, key: &K, value: &V, flags: u64) -> Result<(), c_long> {
-        insert(&mut self.def, key, value, flags)
-    }
-
-    #[inline]
-    pub fn remove(&mut self, key: &K) -> Result<(), c_long> {
-        remove(&mut self.def, key)
+    pub fn remove(&self, key: &K) -> Result<(), c_long> {
+        remove(self.def.get(), key)
     }
 }
 
 #[repr(transparent)]
 pub struct LruPerCpuHashMap<K, V> {
-    def: bpf_map_def,
+    def: UnsafeCell<bpf_map_def>,
     _k: PhantomData<K>,
     _v: PhantomData<V>,
 }
 
+unsafe impl<K: Sync, V: Sync> Sync for LruPerCpuHashMap<K, V> {}
+
 impl<K, V> LruPerCpuHashMap<K, V> {
     pub const fn with_max_entries(max_entries: u32, flags: u32) -> LruPerCpuHashMap<K, V> {
         LruPerCpuHashMap {
-            def: build_def::<K, V>(
+            def: UnsafeCell::new(build_def::<K, V>(
                 BPF_MAP_TYPE_LRU_PERCPU_HASH,
                 max_entries,
                 flags,
                 PinningType::None,
-            ),
+            )),
             _k: PhantomData,
             _v: PhantomData,
         }
@@ -184,35 +192,30 @@ impl<K, V> LruPerCpuHashMap<K, V> {
 
     pub const fn pinned(max_entries: u32, flags: u32) -> LruPerCpuHashMap<K, V> {
         LruPerCpuHashMap {
-            def: build_def::<K, V>(
+            def: UnsafeCell::new(build_def::<K, V>(
                 BPF_MAP_TYPE_LRU_PERCPU_HASH,
                 max_entries,
                 flags,
                 PinningType::ByName,
-            ),
+            )),
             _k: PhantomData,
             _v: PhantomData,
         }
     }
 
     #[inline]
-    pub fn get(&mut self, key: &K) -> Option<&V> {
-        get(&mut self.def, key)
+    pub fn get(&self, key: &K) -> Option<&V> {
+        get(self.def.get(), key)
     }
 
     #[inline]
-    pub fn get_mut(&mut self, key: &K) -> Option<&mut V> {
-        get_mut(&mut self.def, key)
+    pub fn insert(&self, key: &K, value: &V, flags: u64) -> Result<(), c_long> {
+        insert(self.def.get(), key, value, flags)
     }
 
     #[inline]
-    pub fn insert(&mut self, key: &K, value: &V, flags: u64) -> Result<(), c_long> {
-        insert(&mut self.def, key, value, flags)
-    }
-
-    #[inline]
-    pub fn remove(&mut self, key: &K) -> Result<(), c_long> {
-        remove(&mut self.def, key)
+    pub fn remove(&self, key: &K) -> Result<(), c_long> {
+        remove(self.def.get(), key)
     }
 }
 
@@ -229,28 +232,19 @@ const fn build_def<K, V>(ty: u32, max_entries: u32, flags: u32, pin: PinningType
 }
 
 #[inline]
-fn get<'a, K, V>(def: &mut bpf_map_def, key: &K) -> Option<&'a V> {
+fn get<'a, K, V>(def: *mut bpf_map_def, key: &K) -> Option<&'a V> {
     unsafe {
-        let value = bpf_map_lookup_elem(def as *mut _ as *mut _, key as *const _ as *const c_void);
+        let value = bpf_map_lookup_elem(def as *mut _, key as *const _ as *const c_void);
         // FIXME: alignment
         NonNull::new(value as *mut V).map(|p| p.as_ref())
     }
 }
 
 #[inline]
-fn get_mut<'a, K, V>(def: &mut bpf_map_def, key: &K) -> Option<&'a mut V> {
-    unsafe {
-        let value = bpf_map_lookup_elem(def as *mut _ as *mut _, key as *const _ as *const c_void);
-        // FIXME: alignment
-        NonNull::new(value as *mut V).map(|mut p| p.as_mut())
-    }
-}
-
-#[inline]
-fn insert<K, V>(def: &mut bpf_map_def, key: &K, value: &V, flags: u64) -> Result<(), c_long> {
+fn insert<K, V>(def: *mut bpf_map_def, key: &K, value: &V, flags: u64) -> Result<(), c_long> {
     let ret = unsafe {
         bpf_map_update_elem(
-            def as *mut _ as *mut _,
+            def as *mut _,
             key as *const _ as *const _,
             value as *const _ as *const _,
             flags,
@@ -260,8 +254,7 @@ fn insert<K, V>(def: &mut bpf_map_def, key: &K, value: &V, flags: u64) -> Result
 }
 
 #[inline]
-fn remove<K>(def: &mut bpf_map_def, key: &K) -> Result<(), c_long> {
-    let ret =
-        unsafe { bpf_map_delete_elem(def as *mut _ as *mut _, key as *const _ as *const c_void) };
+fn remove<K>(def: *mut bpf_map_def, key: &K) -> Result<(), c_long> {
+    let ret = unsafe { bpf_map_delete_elem(def as *mut _, key as *const _ as *const c_void) };
     (ret >= 0).then(|| ()).ok_or(ret)
 }
